846782575cfb872abe99abb381c553c414c43181
henokwel <henokwel@outlook.com>
2021-12-12 19:54:26 +0100

    removed mui package

--- src/App.js
+++ src/App.js
@@ -29,6 +29,19 @@ function App() {
 
   // handle filter button
 
+
+  //  ==> check if user's device
+
+  
+  // if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  //   // true for mobile device
+  //  console.log("mobile device");
+  // }else{
+  //   // false for not mobile device
+  //  console.log("not mobile device");
+  // }
+
+  
   const handleFilterBtn = (id) => {
 
 
